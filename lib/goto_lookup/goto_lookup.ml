3b70f72eb32af27a333176236876a501c6d5b143
Ronny Krone <47992493+MyLittlePenguin@users.noreply.github.com>
2026-01-02 10:15:38 +0100

    minor bug fix

--- lib/goto_lookup/goto_lookup.ml
+++ lib/goto_lookup/goto_lookup.ml
@@ -118,13 +118,14 @@ let otherwise fn list = function None -> fn list | Some v -> Some v
 
 let rec find (query : query) (list : string list) =
   match query with
+  | Single { ignore_case; needle = "" } -> Some ""
   | Single { ignore_case; needle } ->
       let prepare = get_preparator ignore_case in
       let prepared_needle = prepare needle in
       let contains line = contains prepared_needle (prepare line) in
       let find_some list = find_with contains list in
-      if needle = "" then exit 400
-      else
+      (* if needle = "" then exit 400 *)
+      (* else *)
         None
         |> otherwise (find_perfect prepare needle) list
         |> otherwise (find_end prepare needle) list
