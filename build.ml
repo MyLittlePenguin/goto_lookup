9ec4417c2bc4ede8834e93dfdb5b8fd8620aba9e
Ronny Krone <47992493+MyLittlePenguin@users.noreply.github.com>
2026-05-18 09:27:42 +0200

    add cross compilation for windows

--- build.ml
+++ build.ml
@@ -6,6 +6,7 @@ let rec run = function
   | "" | "build" | "compile" ->
     cmd "cargo build";
   | "release" -> cmd "cargo build --release";
+  | "windows" -> cmd "cargo build --release --target=x86_64-pc-windows-gnu"
   | "install" -> cmd "cargo install --path .";
   | "test" -> cmd "cargo test";
   | "pull" -> cmd "git pull";
